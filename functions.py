7fb309585a8c034e686dbfc70082014b67b74cb0
Luke Alford <lukealford06@gmail.com>
2024-11-13 10:41:23 +0000

    Import functions.py and set up folder

---
+++ functions.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+def mydata(studentID):
+    # Generate a series of 6 data points, dependent on the
+    # input argument studentID
+
+    # DO NOT MODIFY THIS CODE
+    # otherwise your coursework functions might not
+    # work correctly when tested
+
+    # This code is deliberately obscure - you are not
+    # expected to understand it or even look at it!
+
+    assert studentID < 1e9, "studentID exceeds maximum allowed"
+
+    studentID += int(1e8)
+    tmp = str(studentID)
+
+    T = []
+    for k in tmp:
+        T.append(float(k))
+    T = np.array(T)
+    xoff = sum(T)/17 % 2 - 1
+    x = np.linspace(0, 5, 6)
+    x += 0.01 * x * x + xoff
+    yoff = 0.1 + 0.5 * sum(T**2)/13
+    kperm = [7, 5, 8, 4, 6, 1]
+    p = [2, 3, 5, 4, 6, 1] + T[7]
+    y = abs(T[kperm] - yoff) * (-1)**p
+
+    return x, y
