be745363facf9d3275e9b1373af17a1c11ccb905
Luke Alford <lukealford06@gmail.com>
2024-11-14 11:21:24 +0000

    Plots cubic on gragh

--- functions.py
+++ functions.py
@@ -36,6 +36,7 @@ def myID ():
 def cubicfit (x, y, xhat):
     x1,x2,x3,x4 = x[0],x[1],x[2],x[3]
     y1,y2,y3,y4 = y[0],y[1],y[2],y[3]
+
     def L1(xhat): # where x and y are the points, value is the x part of L(x)
         return ((xhat-x2)*(xhat-x3)*(xhat-x4))/((x1-x2)*(x1-x3)*(x1-x4))
     def L2(xhat): # where x and y are the points, value is the x part of L(x)
@@ -44,7 +45,6 @@ def cubicfit (x, y, xhat):
         return ((xhat-x1)*(xhat-x2)*(xhat-x4))/((x3-x1)*(x3-x2)*(x3-x4))
     def L4(xhat): # where x and y are the points, value is the x part of L(x)
         return ((xhat-x1)*(xhat-x2)*(xhat-x3))/((x4-x1)*(x4-x2)*(x4-x3))
-    
     
     yhat = (y1*L1(xhat)) + (y2*L2(xhat)) + (y3*L3(xhat)) + (y4*L4(xhat))
     return yhat
--- plotmydata.py
+++ plotmydata.py
@@ -2,7 +2,16 @@ import matplotlib.pyplot as plt
 from functions import *
 
 x,y = mydata(myID())
-plt.plot(x,y,"X")
+plt.plot(x,y,"rX")
+
+px = np.linspace(x[1],x[4],100)
+py = []
+index = 0
+for count in px:
+    py.append(cubicfit(x[1:5],y[1:5],px[index]))
+    index = index + 1
+plt.plot(px,py)
+
 plt.xlabel("X - axis")
 plt.ylabel("Y - axis")
 plt.title("Plot of Points")
