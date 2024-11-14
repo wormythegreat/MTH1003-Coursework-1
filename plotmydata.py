ad7d3c28d13b6785fa454356c0409f78814500ca
Luke Alford <lukealford06@gmail.com>
2024-11-14 17:57:48 +0000

    Added the legend to the .png

--- plotmydata.py
+++ plotmydata.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from functions import *
 
 x,y = mydata(myID())
-plt.plot(x,y,"rX")
+plt.plot(x,y,"rX", label="Points")
 
 px = np.linspace(x[1],x[4],100)
 py = []
@@ -10,9 +10,10 @@ index = 0
 for count in px:
     py.append(cubicfit(x[1:5],y[1:5],px[index]))
     index = index + 1
-plt.plot(px,py)
+plt.plot(px,py,label="Cubic Function")
 
 plt.xlabel("X - axis")
 plt.ylabel("Y - axis")
+plt.legend()
 plt.title("Plot of Points")
 plt.savefig("./plot.png")
