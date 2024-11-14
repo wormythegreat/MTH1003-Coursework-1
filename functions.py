60033e8084f034ddad44eca21e197d7d87e696f9
Luke Alford <lukealford06@gmail.com>
2024-11-14 10:27:42 +0000

    Moved L function into cubic

--- functions.py
+++ functions.py
@@ -33,8 +33,7 @@ def mydata(studentID):
 def myID ():
     return 740043893
 
-def L(x,y,n,value): # where x and y are the points, n is the nth lagrange polynomial and value is the x part of L(x)
-    pass
-
 def cubicfit (x, y, xhat):
+    def L(x,y,n,value): # where x and y are the points, n is the nth lagrange polynomial and value is the x part of L(x)
+        pass
     pass
