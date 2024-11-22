15aa1e07bf959c0dfeebf969aeb779139036bed3
Luke Alford <lukealford06@gmail.com>
2024-11-22 18:28:46 +0000

    Added comments to all files

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+#This file contains the functions need to plot the data for Coursework 1
+
+#import numpy required for mydata() function
 import numpy as np
 
 def mydata(studentID):
@@ -31,34 +34,63 @@ def mydata(studentID):
     return x, y
 
 def myID ():
-    return 740043893
+    #This is my Student ID so should match: 740043893
+    ID = 740043893
+    return ID
 
 def cubicfit (x, y, xhat):
+    #Takes in x and y arrays of length 4 each and xhat which is the x coord of the point you want to find
+
+    #First sets up some varibles names to make it easier to read and write formulas
     x1,x2,x3,x4 = x[0],x[1],x[2],x[3]
     y1,y2,y3,y4 = y[0],y[1],y[2],y[3]
 
-    def L1(xhat): # where x and y are the points, value is the x part of L(x)
+
+    #These are each of the Lagrange polynomials
+    #Each one is specific to its specified x coord
+    #So L2() gives 1 at x2 and 0 at x1, x3 and x4
+    def L1(xhat):
         return ((xhat-x2)*(xhat-x3)*(xhat-x4))/((x1-x2)*(x1-x3)*(x1-x4))
-    def L2(xhat): # where x and y are the points, value is the x part of L(x)
+    def L2(xhat):
         return ((xhat-x1)*(xhat-x3)*(xhat-x4))/((x2-x1)*(x2-x3)*(x2-x4))
-    def L3(xhat): # where x and y are the points, value is the x part of L(x)
+    def L3(xhat):
         return ((xhat-x1)*(xhat-x2)*(xhat-x4))/((x3-x1)*(x3-x2)*(x3-x4))
-    def L4(xhat): # where x and y are the points, value is the x part of L(x)
+    def L4(xhat):
         return ((xhat-x1)*(xhat-x2)*(xhat-x3))/((x4-x1)*(x4-x2)*(x4-x3))
     
+    #Finally returns the formula for the Lagrange polynomials
     yhat = (y1*L1(xhat)) + (y2*L2(xhat)) + (y3*L3(xhat)) + (y4*L4(xhat))
     return yhat
 
 def findroot(xL, xR):
+    #Takes in a starting guess for the left and right bracket
+    #The y values of these x points will have to be off opposite signs
+
+    #Importing the data to use
     datX,datY = mydata(myID())
+    #This creates a short hand function to quickly find f(x) rather than having to write out cubicfit(datX[1:5],datY[1:5],x) every time
     def f(x):
         return cubicfit(datX[1:5],datY[1:5],x)
+    
+    #This is just the first instance of the error, later it is calculated off xN
     error = abs(f(xL))
-    while error >= 10e-5:
-        xN = ((xL*f(xR))-(xR*f(xL)))/(f(xR)-f(xL))
+
+    #This while loop states while the guess is further than (10)^-5 then it wil find a xN between xL and xR
+    while error >= 10**(-5):
+        
+        #Uses the Regula Falsi formula to find xN
+        xN = ( (xL*f(xR)) - (xR*f(xL))  )   /   (f(xR) - f(xL))
+
+        #Now it retains which ever bracket has the same sign as f(xN)
         if f(xN)*f(xL) <= 0:
             xR = xN
         else:
             xL=xN
+
+        #Calculates the error based off xN
+        #(Side note - originally i had this based off xL but under a couple of very specific tests it would not work as xR would be exactly
+        #  on the root but it would take python a very very long time to slowly get the left bracket close enough to be within the error)
         error = abs(f(xN))
+    
+    #Finally returns the value once the error is small enough and it breaks out of the loop
     return xL
--- plotmydata.py
+++ plotmydata.py
@@ -1,27 +1,60 @@
+#This file creates a plot of 6 points, a cubic function between 4 of them, and the root of that cubic
+
+#import matplotlib for plotting
 import matplotlib.pyplot as plt
+#import all functions from the previous file
 from functions import *
 
+
+#Retrieves the data for the plot
 x,y = mydata(myID())
 
-px = np.linspace(x[1],x[4],100)
+
+#This next section creates 2 arrays of size 100 which contain the x and y coords respectively of points in the cubic
+#First an array of 100 equally sized points between the 2nd and 5th data point
+px = np.linspace(x[1],x[4],100) #numpy can be used here because it was imported when functions was imported
+#Sets up the corrosponding y array which is empty to start
 py = []
+#Starts the index at 0
 index = 0
+#Loops through each of the values in px which is the x coords adds the matching y coord to the y array
 for count in px:
+    #This appends to the end of the y array and gets the y value from the cubic fit function and passes in the x coord
     py.append(cubicfit(x[1:5],y[1:5],px[index]))
+    #Adds one to the index for each loop
     index = index + 1
 
+
+#This section finds the x and y coord of the root of the cubic equation
+#The x coord is found by using the findroot() function explained in functions.py
+#The starting guess here is the 3rd and 4th point
 rootX = findroot(x[2],x[3])
+#The y coord can then be found by applying the cubic function at rootX
 rootY = cubicfit(x[1:5],y[1:5],rootX)
 
 
+#This section plots all of the points and lines onto the graph
+#.axhline() plots a horizontal line at y=0 basically adding an x-axis
 plt.axhline(0,0,1,color="grey")
+#.axvline() plots a vertical line at x=0 basically adding a y-axis
 plt.axvline(0,0,1,color="grey")
+#This plots the array of x and y that were made using the cubic function
 plt.plot(px,py,label="Cubic Function")
+#This plots the original 6 points got from mydata()
+#The reason this is plotted after the cubic funciton is so that the points appear
+# on top of the line and can be seen more easily
 plt.plot(x,y,"ro", label="Points")
+#This plots a single point which is the root where the cubic graph crosses the x-axis
 plt.plot(rootX,rootY,"o",color="black",label="Root")
 
+
+#This final section adds labels and saves the graph
+#These add lables to the x and y axis
 plt.xlabel("X - axis")
 plt.ylabel("Y - axis")
+#This adds the legend to the graph
 plt.legend()
+#Adds a title
 plt.title("Plot of Points")
+#Saves the graph to this directory with . being the current directory
 plt.savefig("./plot.png")
